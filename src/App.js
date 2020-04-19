e2450c67f5047278e6f07af7d1f84f9387fb9d01
Mike Ludemann <mike-ludemann@hotmail.de>
2020-04-19 19:50:38 +0200

    Setup complete - react-hooks-components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+import Accordion from './components/accordion/accordion';
+
+function App() {
+	return (
+		<div className="App">
+      <Accordion
+        title="Welcome stranger"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="How can I help you?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="Did you need help for a problem?"
+        content="
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        "
+      />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/accordion/accordion.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+import Arrow from "./arrow";
+
+import "./accordion.css";
+
+function Accordion(props) {
+	const [setActive, setActiveState] = useState("");
+	const [setHeight, setHeightState] = useState("0px");
+	const [setRotate, setRotateState] = useState("accordion--icon");
+
+	const content = useRef(null);
+
+	function toggleAccordion() {
+		setActiveState(setActive === "" ? "active" : "");
+		setHeightState(
+			setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+		);
+		setRotateState(
+			setActive === "active" ? "accordion--icon" : "accordion--icon rotate"
+		);
+	}
+
+	return (
+		<div className="accordion--section">
+			<button className={`accordion ${setActive}`} onClick={toggleAccordion}>
+				<p className="accordion--title">{props.title}</p>
+				<Arrow className={`${setRotate}`} width={10} fill={"#777"} />
+			</button>
+			<div
+				ref={content}
+				style={{ maxHeight: `${setHeight}` }}
+				className="accordion--content"
+			>
+				<div
+					className="accordion--text"
+					dangerouslySetInnerHTML={{ __html: props.content }}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Accordion;
